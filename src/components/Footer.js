8dec3eb20b59604aa3b2cdc35e5b3013e68d46cb
Khushal7773 <khushalsinghpanwar07@gmail.com>
2023-08-19 14:32:12 +0530

    layout is ready

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+function Footer() {
+    return (
+        <footer className=" bg-gray-800 text-white py-6">
+        <div className="container mx-auto text-center">
+          &copy; 2023 My Landing Page. All rights reserved.
+        </div>
+      </footer>
+    )
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import HeaderLogo from '../assets/logo.png';
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 z-50 bg-white p-4 shadow">
+      <div className="container mx-auto flex justify-between items-center">
+  
+        <img
+          src={HeaderLogo} 
+          alt="Logo"
+          className='rounded-lg'
+          
+        />
+
+        
+        <nav>
+          <ul className="flex space-x-4">
+            <li className='mx-6'><a href="#">Emotions</a></li>
+            <li className='mx-6'><a href="#">Manifesto</a></li>
+            <li className='mx-6'><a href="#">Self-awareness test</a></li>
+            <li className='mx-6'><a href="#">Work With Us</a></li>
+          </ul>
+        </nav>
+
+  
+        <button className="bg-black text-white px-4 py-2 rounded-full hover:bg-blue-600 transition-colors duration-300">
+          Download app
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
